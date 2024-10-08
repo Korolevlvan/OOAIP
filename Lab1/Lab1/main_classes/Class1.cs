dcdacab4765fd31b88fdc85f4a639e48f0f4ebcf
Azazel <93066733+Korolevlvan@users.noreply.github.com>
2024-10-08 16:26:06 +0600

    Создание интерфейсов ICommand, IMove, Классов Vector, CMove, Конфиг .editorconfig

---
+++ Lab1/Lab1/main_classes/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1.main_classes
+{
+    public interface ICommand
+    {
+        public void Execute();
+    }
+}
---
+++ Lab1/Lab1/main_classes/Move.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1.main_classes
+{
+    public interface IMovable
+    {
+        public int[] Position { get; set; }
+        public int[] Velocity { get; }
+    }
+
+    public class CMove : ICommand
+    {
+        private readonly IMovable movable;
+        public CMove(IMovable movable)
+        {
+            this.movable = movable;
+        }
+        public void Execute()
+        {
+            movable.Position = new int[]{
+            movable.Position[0] +  movable.Velocity[0],
+            movable.Position[1] +  movable.Velocity[1],
+        };
+        }
+    }
+}
