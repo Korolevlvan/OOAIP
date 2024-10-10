70b236e2c4f00f3bf3551e46f3b767759561373b
Azazel <93066733+Korolevlvan@users.noreply.github.com>
2024-10-10 08:02:07 +0600

    Созлание IUObject, IMoveStartCommand

---
+++ Lab2/Lab2/IUObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    internal interface IUObject
+    {
+        void set_property(string key, object value);
+        object get_property(string key);
+    }
+}
