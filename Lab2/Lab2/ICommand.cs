3d72ed5f3279a21f5f22576918ea9f2ecf36e578
Azazel <93066733+Korolevlvan@users.noreply.github.com>
2024-10-10 08:31:40 +0600

    Создание ICommand, startMoveCommand

--- Lab2/Lab2/ICommand.cs
+++ Lab2/Lab2/ICommand.cs
@@ -3,13 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Input;
 
 namespace Lab2
 {
-    internal interface IQueue
+    public interface ICommand
     {
-        void Put(ICommand cmd);
-        ICommand Take();
+        public void Execute();
     }
 }
---
+++ Lab2/Lab2/StartMoveCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hwdtech;
+namespace Lab2
+{
+    internal class StartMoveCommand: ICommand
+    {
+        private readonly IStartMoveCommand _start;
+
+        public StartMoveCommand(IStartMoveCommand start) => _start = start;
+
+        public void Execute()
+        {
+
+            _start.Properties.ToList().ForEach(a => IoC.Resolve<ICommand>("Properties.Set", _start.Target, a.Key, a.Value).Execute());
+            var cmd = IoC.Resolve<ICommand>("Operations.Movement", _start.Target);
+            var injectable = IoC.Resolve<ICommand>("Commands.Injectable", cmd);
+            IoC.Resolve<ICommand>("Properties.Set", _start.Target, "Operations.Movement", cmd).Execute();
+            IoC.Resolve<IQueue>("Game.Queue").Put(injectable);
+        }
+    }
+}
