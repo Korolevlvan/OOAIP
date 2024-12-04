d5c12f6789d594089239723f80363f5a2b372daf
Azazel <93066733+Korolevlvan@users.noreply.github.com>
2024-12-04 10:39:33 +0600

    Создание StartMoveCommand

---
+++ Lab1/Lab.1/StartMoveCommand.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Hwdtech;
+
+namespace Lab1
+{
+  public class StartMoveCommand : ICommand
+  {
+    private readonly IMoveCommandStartable _startable;
+
+    public StartMoveCommand(IMoveCommandStartable startable) => _startable = startable;
+
+    public void Execute()
+    {
+
+      _startable.Properties.ToList().ForEach(a => IoC.Resolve<ICommand>("Properties.Set", _startable.Target, a.Key, a.Value).Execute());
+      var cmd = IoC.Resolve<ICommand>("Operations.Movement", _startable.Target);
+      var injectable = IoC.Resolve<ICommand>("Commands.Injectable", cmd);
+      IoC.Resolve<ICommand>("Properties.Set", _startable.Target, "Operations.Movement", cmd).Execute();
+      IoC.Resolve<IQueue>("Game.Queue").Put(injectable);
+    }
+  }
+}
